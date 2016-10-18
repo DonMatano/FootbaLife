3a16d269f5c200483dfcf52331625f5d7c913ad7
DonMatano <Mwakimamatano@gmail.com>
2016-10-18 15:00:24 +0530

    Adds the user profile pic

--- app/src/main/java/com/matano/footbalife/TweetsAdapter.java
+++ app/src/main/java/com/matano/footbalife/TweetsAdapter.java
@@ -1,12 +1,22 @@
 package com.matano.footbalife;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 import twitter4j.Status;
@@ -18,6 +28,7 @@ import twitter4j.Status;
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>
 {
     List<Status> mStatuses;
+    final String TAG = TweetsAdapter.class.getSimpleName();
 
     //When object created we give the list of statuses
     public TweetsAdapter(List<Status> statuses)
@@ -44,11 +55,14 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
     @Override
     public void onBindViewHolder(TweetsAdapter.ViewHolder holder, int position)
     {
-
-
         holder.usernameView.setText(mStatuses.get(position).getUser().getName());
         holder.userHandleView.setText("@" + mStatuses.get(position).getUser().getScreenName());
         holder.tweetText.setText(mStatuses.get(position).getText());
+        holder.mDownloadUserProfilePic = new DownloadUserProfilePic(
+                mStatuses.get(position).getUser().getProfileImageURL(),
+                holder);
+        holder.mDownloadUserProfilePic.execute();
+
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder
@@ -57,6 +71,8 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         public TextView usernameView;
         public TextView userHandleView;
         public TextView tweetText;
+        public ImageView userProfilePic;
+        DownloadUserProfilePic mDownloadUserProfilePic;
 
         public ViewHolder(View itemView)
         {
@@ -66,6 +82,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             usernameView = (TextView) itemView.findViewById(R.id.username_textView);
             userHandleView = (TextView) itemView.findViewById(R.id.username_handle_textview);
             tweetText = (TextView) itemView.findViewById(R.id.text_tweetView);
+            userProfilePic = (ImageView) itemView.findViewById(R.id.user_profileImage);
         }
     }
 
@@ -73,5 +90,51 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
     {
         mStatuses = statuses;
         notifyDataSetChanged();
+    }
+
+    private static Bitmap getImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            Log.e("Method getImageBitmap", "Error getting bitmap", e);
+        }
+        return bm;
+    }
+
+
+    /**
+     * Created by M.Matano on 18-Oct-16.
+     */
+
+    public static class DownloadUserProfilePic extends AsyncTask<Void, Void, Bitmap>
+    {
+        private String url;
+        private ViewHolder vh;
+
+        public DownloadUserProfilePic(String url, ViewHolder vh)
+        {
+            this.url = url;
+            this.vh = vh;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void[] params)
+        {
+            return TweetsAdapter.getImageBitmap(url);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result)
+        {
+            vh.userProfilePic.setImageBitmap(result);
+        }
     }
 }
